cb1ab58387c50a0dd78af556eb29dab61159e52b
wsl-thomas <thomasthienbui@gmail.com>
2022-04-26 14:16:12 +0700

    Finished section 2

---
+++ src/acorn.c
@@ -0,0 +1,59 @@
+/*** includes ***/
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+/*** data ***/
+struct termios default_termios;
+
+/*** terminal ***/
+void die(const char* s) {
+    perror(s);
+    exit(1);
+}
+
+void disable_raw_mode() {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &default_termios) == -1) {
+        die("tcsetattr");
+    }
+}
+
+void enable_raw_mode() {
+    if (tcgetattr(STDIN_FILENO, &default_termios) == -1) {
+        die("tcgetattr");
+    }
+    atexit(disable_raw_mode);
+
+    struct termios raw = default_termios;
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+        die("tcsetattr");
+    }
+}
+/*** init ***/
+int main() {
+    enable_raw_mode();
+    while (1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+            die("read");
+        }
+        if (iscntrl(c)) {
+            printf("%d\n\r", c);
+        } else {
+            printf("%d ('%c')\n\r", c, c);
+        }
+        if (c == 'q') break;
+    }
+    return 0;
+}
