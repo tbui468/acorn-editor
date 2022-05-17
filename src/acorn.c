af048b624b35d04d038801c36ee744844d577d31
wsl-thomas <thomasthienbui@gmail.com>
2022-05-17 14:48:36 +0700

    Nonprintable characters

--- src/acorn.c
+++ src/acorn.c
@@ -737,7 +737,18 @@ void editor_draw_rows(struct AppendBuffer* ab) {
             int current_color = -1;
             int j;
             for (j = 0; j < len; j++) {
-                if (hl[j] == HL_NORMAL) {
+                if (iscntrl(c[j])) {
+                    char sym = (c[j] <= 26) ? '@' + c[j] : '?';
+                    append_buffer_append(ab, "\x1b[7m", 4);
+                    append_buffer_append(ab, &sym, 1);
+                    append_buffer_append(ab, "\x1b[m", 3);
+                    //reset current color
+                    if (current_color != -1) {
+                        char buf[16];
+                        int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", current_color);
+                        append_buffer_append(ab, buf, clen);
+                    }
+                } else if (hl[j] == HL_NORMAL) {
                     if (current_color != -1) {
                         append_buffer_append(ab, "\x1b[39m", 5);
                         current_color = -1;
